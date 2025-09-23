22c7d81b0cdc86bdaea44685a9abc3fe160320ef
Alex Ru <alexander.rudel@web.de>
2025-09-23 17:16:34 +0300

    rand selection
    random selection for computer between rock, paper, scissors

--- RockPaperScissors/roPaSc.js
+++ RockPaperScissors/roPaSc.js
@@ -1,4 +1,16 @@
 
 
+function getComputerChoice(){
 
-console.log("Hello World!");
+    const rand = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+    switch(rand){
+        case 1: return "Rock";
+        case 2: return "Paper";
+        default: return "Scissors";
+    }
+
+
+}
+
+console.log(getComputerChoice());
