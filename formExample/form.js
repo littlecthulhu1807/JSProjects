24144b9e0500c54266153525b838d502ad60f831
Alex Ru <alexander.rudel@web.de>
2025-10-02 18:51:26 +0300

    Form Practice
    Set up a form page to subscribe to an imaginary service

---
+++ formExample/form.js
@@ -0,0 +1,5 @@
+const password = document.querySelector("#password");
+const cPassword = document.querySelector("#cPassword");
+
+cPassword.setCustomValidity("This field is required.");
+password.setCustomValidity("This field is required.");
---
+++ formExample/readme.txt
@@ -0,0 +1,3 @@
+Photo by Nathan Ziemanski on Unsplash
+https://unsplash.com/photos/water-falls-on-forest-hjW4o5XfCzc
+      
